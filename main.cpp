eb015d475f9870d2f11de51184df955e2cb3ccb3
Roman Milishchuk <milromchuk@gmail.com>
2019-12-29 13:14:39 +0200

    added struct for directory entry

--- main.cpp
+++ main.cpp
@@ -13,6 +13,25 @@ struct boot_sector {
 
 };
 
+typedef struct {
+    char filename[11];
+    uint8_t attributes;
+    uint8_t reserved;
+    uint8_t creation_time_ms;
+    uint16_t creat_time;
+    uint16_t creat_date;
+    uint16_t access_date;
+    uint16_t first_cluster_addr_high;
+    uint16_t modified_time;
+    uint16_t modified_date;
+    uint16_t first_cluster_addr_low;
+    uint32_t file_size;
+} dir_entry;
+
+void print_directory_info(dir_entry *entry) {
+    
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
